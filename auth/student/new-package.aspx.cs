3b72e4e1347c46b4a90be9aa345ec31bba7a3628
SarcasticSigma <SarcasticSigma@gmail.com>
2022-04-23 04:11:58 -0400

    Fixed merge issues.

--- auth/student/new-package.aspx.cs
+++ auth/student/new-package.aspx.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
 namespace CapstoneWebPage.auth.student
+
+
+
 {
     public partial class new_package : System.Web.UI.Page
     {
@@ -35,7 +39,7 @@ namespace CapstoneWebPage.auth.student
                         {
                             System.Diagnostics.Debug.WriteLine(reader.ToString());
                             Guid userId = reader.GetGuid(0);
-                            return userId.ToString();   
+                            return userId.ToString();
                         }
                     }
                     reader.Close();
@@ -126,74 +130,5 @@ namespace CapstoneWebPage.auth.student
             }
         }
 
-        /// <summary>
-        /// Inserts a new package into the database corresponding to the current user. 
-        /// </summary>
-        /// <returns>Returns a boolean describing whether or not the insertion was a success.</returns>
-        bool InsertNewPackage(string expectedArrivalDate, string trackingNumber, int ShippingCompanyId)
-        {
-            string expectedDate = expectedArrivalDate;
-            string stringTrackingNumber = trackingNumber;
-            int i = 0;
-
-            String myConString = System.Configuration.ConfigurationManager.ConnectionStrings["MainSquireDatabase"].ConnectionString;
-            String mySQL = "INSERT INTO Packages () VALUES (@)";
-            using (SqlConnection myCon = new SqlConnection(myConString))
-            {
-                myCon.Open();
-                using (SqlCommand myCom = new SqlCommand(mySQL, myCon))
-                {
-
-
-                }
-            }
-
-            return false;
-        }
-
-        protected void btnSubmit_Click(object sender, EventArgs e)
-        {
-            Page.Validate();
-            if (Page.IsValid)
-            {
-                if (InsertNewPackage(txtExpectedDate.Text, txtTrackingNumber.Text, System.Convert.ToInt16(ddlShippingCompany.SelectedItem.Value)))
-                {
-                    txtExpectedDate.Text = "";
-                    txtTrackingNumber.Text = "";
-                    ddlShippingCompany.SelectedIndex = 0;
-                }
-                else
-                {
-                    lblOutput.Text = "Error inserting package! Please try again!";
-                }
-            }
-
-        }
-
-        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
-        {
-            if (args.Value == "")
-            {
-                args.IsValid = false;
-            }
-            Regex rg = new Regex("[0-9][0-9](/|-|\\ )[0-9][0-9](/|-|\\ )[0-9][0-9][0-9][0-9]");
-            if (rg.IsMatch(args.Value))
-            {
-                char[] targetDate = args.Value.ToCharArray();
-                targetDate[2] = '/';
-                targetDate[5] = '/';
-                string targetDateString = targetDate.ToString();
-
-                if (DateTime.TryParse(targetDateString, out _))
-                {
-                    args.IsValid = true;
-                }
-
-            }
-            else
-            {
-                args.IsValid = false;
-            }
-        }
     }
 }
